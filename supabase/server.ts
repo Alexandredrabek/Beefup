4b51e5d52e4a73b0278cb6fdddc977c262429881
Alexandredrabek <drabekalexandre@gmail.com>
2026-03-26 00:10:23 +0100

    Update server.ts

--- supabase/server.ts
+++ supabase/server.ts
@@ -9,12 +9,16 @@ export async function createClient() {
     {
       cookies: {
         getAll() { return cookieStore.getAll() },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
+            cookiesToSet.forEach(({ name, value, options }: any) =>
               cookieStore.set(name, value, options)
             )
-          } catch {}
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     }
